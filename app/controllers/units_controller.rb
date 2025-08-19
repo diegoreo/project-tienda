2fc6b21a916558a3ca33a1453aed671d9b034987
diegoreo <diego1_reo@hotmail.com>
2025-08-19 17:52:21 -0600

    crud category, unit
    se creo crud para category y unit, se crearon sus test de category y unit se creo un doc css para los formularios de category y unit categories.css

---
+++ app/controllers/units_controller.rb
@@ -0,0 +1,47 @@
+class UnitsController < ApplicationController
+  def index
+    @units = Unit.all
+  end
+
+  def new
+    @unit = Unit.new
+  end
+
+  def create
+    @unit = Unit.new(unit_params) 
+    if @unit.save
+      redirect_to units_url, notice: "Tu unidad de medida se creo correctamente!"
+    else
+      render :new, status: :unprocessable_content
+    end
+  end
+
+  def edit
+    unit
+  end
+
+  def update
+    if unit.update(unit_params)
+      redirect_to units_url, notice: "tu unidad de medida se actualizo correctamente"
+    else
+      render :edit, status: :unprocessable_content
+    end
+  end
+
+  def destroy
+    unit.destroy
+    redirect_to units_url, notice: "La unidad de medida se elimino correctamente", status: :see_other
+  end
+
+  private
+
+  def unit
+    @unit = Unit.find(params[:id])
+  end
+
+  def unit_params
+    params.require(:unit).permit(
+      :name, :abbreviation, :description
+    )
+  end
+end
---
+++ test/controllers/units_controller_test.rb
@@ -0,0 +1,80 @@
+require "test_helper"
+class UnitsControllerTest < ActionDispatch::IntegrationTest
+  test "render a list of units" do
+    get units_path
+
+    assert_response :success
+    assert_select ".unit-card",6
+  
+  end
+
+  test "render a new category form" do
+    get new_unit_path
+
+    assert_response :success
+    assert_select "form"
+  end
+
+  test "allow to create a new unit" do
+    post units_path, params: {
+      unit: {
+        name: "Gramo",
+        abbreviation: "gr",
+        description: "Unidad de peso gramos, cuando se compra en kilo unidad de venta podria ser gramo"
+      }
+    }
+
+    assert_redirected_to units_path
+    assert_equal flash[:notice], "Tu unidad de medida se creo correctamente!"
+  end
+
+  test "does not allow to create a new unit with empty fields" do
+    post units_path, params: {
+      unit: {
+        name: "",
+        abbreviation: "gr",
+        description: "Unidad de peso gramos, cuando se compra en kilo unidad de venta podria ser gramo"
+      }
+    }
+
+    assert_response :unprocessable_content 
+  end
+
+  test "render an edit unit form" do
+    get edit_unit_path(units(:caja))
+
+    assert_response :success
+    assert_select "form"
+  end
+
+  test "allow to update a unit" do
+    patch unit_path(units(:paquete)), params: {
+      unit: {
+        description: "Conjunto de varias piezas empaquetadas t"
+      }
+    }
+
+    assert_redirected_to units_path
+    assert_equal flash[:notice], "tu unidad de medida se actualizo correctamente"
+  end
+
+  test "does not allow to update a unit with an invalid field" do
+    patch unit_path(units(:caja)), params: {
+      unit: {
+        name: ""
+      }
+    }
+
+    assert_response :unprocessable_content 
+  end
+
+  test "can delete units" do
+    assert_difference("Unit.count", -1) do
+      delete unit_path(units(:litro))
+    end
+    assert_redirected_to units_path
+    assert_equal flash[:notice], "La unidad de medida se elimino correctamente"
+
+  end
+
+end
